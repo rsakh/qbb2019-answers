0ee10066393e74b84031ac46e543df1ca0cd2312
Rima Sakhawala <rsakhaw1@jhu.edu>
2019-08-27 17:07:29 -0400

    hwcode

---
+++ day2-homework/day2-homework.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+for line in (sys.stdin):
+    #filter so that you only get DROME and that the last column contains fbgn because some of the lines in the 4th column are empty
+    if "DROME" and "FBgn" not in line:
+        continue
+    #eliminate the whitespace and returns   
+    fields = line.rstrip("\n").split()
+    #count the columns backwords because the first two columns are not always filled in so it may assume that one less column    
+    uniprot = fields[-2]
+    flybase = fields[-1]
+    
+    print(flybase, "\t", uniprot)
+        
