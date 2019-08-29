59e83509b6687f517119b9d132dcc7869060170f
Rima Sakhawala <rsakhaw1@jhu.edu>
2019-08-29 14:19:44 -0400

    day4 lunch #2

---
+++ day4-lunch/day4_lunchex2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+"""
+Usage: ./day4_lunchex2.py <ctab1> <ctab2> 
+
+Plot FPKM from two sources
+"""
+
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+import pandas as pd
+
+
+name1=sys.argv[1].split(os.sep)[-2]
+ctab1 = pd.read_csv(sys.argv[1], sep = "\t", index_col="t_name")
+
+name2=sys.argv[2].split(os.sep)[-2] 
+ctab2 =pd.read_csv(sys.argv[2], sep = "\t", index_col="t_name")
+
+fpkm = {name1: ctab1.loc[:,"FPKM"],
+        name2: ctab2.loc[:,"FPKM"]}
+        
+df= pd.DataFrame(fpkm)
+
+g_ctab1 = df.loc[:,"SRR072893"]  
+g_ctab2 = df.loc[:,"SRR072894"] 
+
+x = np.log2(g_ctab1 +1)
+y = np.log2(g_ctab2 +1)
+
+fig, ax = plt.subplots()
+ax.scatter( x= x, y= y, s=5, alpha=.15)
+
+plt.title("FPKMS of SRR072893 and SRR072894")
+plt.xlabel("log2(FPKM) of SRR072893")
+plt.ylabel("log2(FPKM) of SRR072894")
+
+line = np.polyfit(x, y, 1)
+xl = np.linspace(0, 10, 10)
+yl = line [0]*xl + line[1]
+ax.plot(xl, yl, label= "curve", color= "red")
+ax.legend()
+
+print(line)
+print(type(line))
+
+fig.savefig("scatter.png")
+plt.close(fig)
+
+
+#
+# fig, ax = plt.subplots() #fig=the entire image, ax=subpanel, so plot two graphs in one image
+# ax.hist(my_data, bins=100, density=True) #default bins=10
+# ax.plot (x,y1, label="skew distribution")
+# ax.plot(x,y2, label="normal")
+# ax.legend()
+
+# plt.text(-15, 0.15, "mu=5.9, sigma=2.65, a=-1.9")
+
+# fig.savefig("fpkms1.png")
+# plt.close(fig)
