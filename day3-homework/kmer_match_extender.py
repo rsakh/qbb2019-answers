3a16667ad32d68b1ca63d4a12b281c75b5ea7970
Rima Sakhawala <rsakhaw1@jhu.edu>
2019-09-02 16:24:13 -0400

    unfinished still

--- day3-homework/kmer_match_extender.py
+++ day3-homework/kmer_match_extender.py
@@ -13,43 +13,45 @@ final_dict = {}
 
 for ident, sequence in target:
     sequence = sequence.upper()
-    extender[ident] = sequence
-    for j in range(0, len(sequence) - k +1):
+    extender[ident] = sequence #making a dictionary with extended kmers? why do we have this here? we dont mention extended kmers until later?
+    for j in range(0, len(sequence) - k +1): # groupings, math problems thing that siran showed me
         kmer = sequence[j:j+k]
         if kmer in target_dict:
-            target_dict[kmer].append((ident,j))
+            target_dict[kmer].append((ident,j)) #if kmer is there, then you are going to add it into the dictionary, but if not there then what is the else doing?
         else:
             target_dict[kmer]=[(ident,j)]
-            
-   # print(target_dict)
+# print(target_dict)
 
 for ident, sequence in query:
     sequence = sequence.upper()
     for i in range(0, len(sequence) - k +1):
         kmer = sequence[i:i+k]
+        #print (kmer)
         if kmer in target_dict:
-            for ident, j in target_dict[kmer]:
-                target_seq = extender[ident]
-                target_length = len(target_seq)
+            for ident, j in target_dict[kmer]: #if the kmer is in the target dict then you want to extend it
+                target_seq = extender[ident] # I'm reassigning the target sequence dictionary here
+                target_length = len(target_seq) #defining a new input for target seq because for statement for query overrides the for statement for target seq?
                 query_length = len(sequence) #query seq
-                extend_right = True
+
+                extend_right = True #assigning the variable as True
                 extended_kmer = kmer
-                while True:
-                    if extend_right:
-                        if sequence[i+k+1] == target_seq[j+k+1]:
-                            i += 1
-                            j += 1
-                            extender_kmer += sequence[i+k+1]
-                        else:
+
+                while extend_right == True:
+                    if sequence[i+k+1] == target_seq[j+k+1]:
+                        i += 1
+                        j += 1
+                        extended_kmer += sequence[i+k+1]
+
+                    elif (j+k == target_length) or (i+k == query_length): #this means that there isn't a matching base next to the kmer? or that it stops?
+                        final_dict[extended_kmer].append((ident), i)
                         extend_right = False
+
                     else:
-                        #this is where i would add to my final dict where I would add my extention
+                        final_dict[extended_kmer].append((ident), i)
+                        extend_right = False # could write break instead, right?
                         break
-                    if (j+k == target_length) or (i+k == query_length):
-                        extend_right = False
-                
-                        
-
-
-
-
+                print(final_dict[extender_kmer])
+        #final_dict[extended_kmer].append((ident), i)
+                #this is where i would add to my final dict where I would add my extention
+                #print (extended_kmer,i)
+                # print (ident, i)
